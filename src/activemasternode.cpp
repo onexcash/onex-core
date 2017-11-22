941db4858a957c2e106091f7029aab0eb2730182
onexcashdev <support@onex.cash>
2017-11-22 20:29:47 +0100

    reduce masternode to 5000

--- src/activemasternode.cpp
+++ src/activemasternode.cpp
@@ -209,8 +209,8 @@ void CActiveMasternode::ManageStateInitial()
         return;
     }
 
-    if(pwalletMain->GetBalance() < 10000 * COIN) {
-        LogPrintf("CActiveMasternode::ManageStateInitial -- %s: Wallet balance is < 10000 ONEX\n", GetStateString());
+    if(pwalletMain->GetBalance() < 5000 * COIN) {
+        LogPrintf("CActiveMasternode::ManageStateInitial -- %s: Wallet balance is < 5000 ONEX\n", GetStateString());
         return;
     }
 
